d88a2737ec6f9a9dc017258b32f8e69cd505da87
Taylor Fausak <taylor@fausak.me>
2019-03-13 22:45:05 -0400

    Start working on the actual RSS feed

--- podcast.hs
+++ podcast.hs
@@ -4,9 +4,25 @@ import qualified System.FilePath as FilePath
 
 main :: IO ()
 main = do
-  let directory = "site"
-  Directory.createDirectoryIfMissing True directory
-  writeFile (FilePath.combine directory "index.html") (concat
+  let input = "input"
+  let output = "output"
+  Directory.createDirectoryIfMissing True output
+
+  Directory.copyFile
+    (FilePath.combine input "logo.png")
+    (FilePath.combine output "haskell-weekly-podcast-logo.png")
+
+  -- https://help.apple.com/itc/podcasts_connect/#/itcbaf351599
+  writeFile (FilePath.combine output "haskell-weekly-podcast.rss") (concat
+    [ "<?xml version='1.0' encoding='utf-8'?>"
+    , "<rss version='2.0' xmlns:itunes='http://www.itunes.com/dtds/podcast-1.0.dtd' xmlns:content='http://purl.org/rss/1.0/modules/content/'>"
+      , "<channel>"
+        , "<title>Haskell Weekly</title>"
+      , "</channel>"
+    , "</rss>"
+    ])
+
+  writeFile (FilePath.combine output "index.html") (concat
     [ "<!doctype html>"
     , "<html>"
       , "<head>"
@@ -16,7 +32,9 @@ main = do
       , "</head>"
       , "<body>"
         , "<h1>Haskell Weekly Podcast</h1>"
-        , "<p>It's coming soon!</p>"
+        , "<p>"
+          , "<a href='haskell-weekly-podcast.rss'>RSS feed</a>"
+        , "</p>"
       , "</body>"
     , "</html>"
     ])
