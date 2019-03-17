32fba38fe61cfe95d57a969f5a42107de02573b1
Taylor Fausak <taylor@fausak.me>
2019-03-17 15:37:35 -0400

    Put some blank lines before the links

--- source/library/Podcast/Episodes.hs
+++ source/library/Podcast/Episodes.hs
@@ -15,7 +15,7 @@ import qualified Podcast.Type.Url as Url
 episodes :: [Either String Episode.Episode]
 episodes =
   [ Episode.Episode
-    <$> Description.fromString "Sara Lichtenstein and Taylor Fausak talk about upgrading from Elm 0.18 to 0.19. https://engineering.itpro.tv/2019/03/01/upgrading-elm-to-v19/"
+    <$> Description.fromString "Sara Lichtenstein and Taylor Fausak talk about upgrading from Elm 0.18 to 0.19.\n\nhttps://engineering.itpro.tv/2019/03/01/upgrading-elm-to-v19/"
     <*> pure (Seconds.fromNatural 899)
     <*> Guid.fromString "00900298-5aa6-4301-a207-619d38cdc81a"
     <*> Number.fromNatural 2
@@ -23,7 +23,7 @@ episodes =
     <*> Time.fromString "2019-03-13T12:00:00"
     <*> Url.fromString "https://user.fm/files/v2-713fb5701a33ecfce9fbd9d407df747f/episode-2.mp3"
   , Episode.Episode
-    <$> Description.fromString "Cody Goodman and Taylor Fausak talk about handling exceptions in Haskell. https://markkarpov.com/tutorial/exceptions.html"
+    <$> Description.fromString "Cody Goodman and Taylor Fausak talk about handling exceptions in Haskell.\n\nhttps://markkarpov.com/tutorial/exceptions.html"
     <*> pure (Seconds.fromNatural 583)
     <*> Guid.fromString "6fe12dba-e0c3-4af5-b9fc-844bc2396ae7"
     <*> Number.fromNatural 1
