55e065808ddb47006afe700b456fcb187cd95733
Taylor Fausak <taylor@fausak.me>
2019-03-17 16:19:13 -0400

    Change "UTC" to "UT" in RFC 822 strings

--- source/library/Podcast/Type/Time.hs
+++ source/library/Podcast/Type/Time.hs
@@ -17,7 +17,7 @@ fromString string = case Time.parseTimeM False locale iso8601 string of
   Just utcTime -> Right (Time utcTime)
 
 toString :: Time -> String
-toString (Time utcTime) = Time.formatTime locale rfc822 utcTime
+toString (Time utcTime) = dropEnd 1 (Time.formatTime locale rfc822 utcTime)
 
 locale :: Time.TimeLocale
 locale = Time.defaultTimeLocale
@@ -27,3 +27,6 @@ iso8601 = Time.iso8601DateFormat (Just "%T")
 
 rfc822 :: String
 rfc822 = Time.rfc822DateFormat
+
+dropEnd :: Int -> [a] -> [a]
+dropEnd n xs = zipWith const xs (drop n xs)
